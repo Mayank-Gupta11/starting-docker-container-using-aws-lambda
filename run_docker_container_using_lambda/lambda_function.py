9860953e85c7b5e356db0ebf74b535510beef9ca
Mayank Gupta <33365090+Mayank-Gupta11@users.noreply.github.com>
2021-06-22 11:29:09 +0530

    made some updates

--- run_docker_container_using_lambda/lambda_function.py
+++ run_docker_container_using_lambda/lambda_function.py
@@ -10,23 +10,7 @@ def lambda_handler(event, context):
     content = s3.Object(my_bucket, my_key)
     file_data = content.get()['Body'].read()
     file_data = file_data.decode('utf-8')
-    file_datadict = json.loads(file_data)
-    
-    flag=file_datadict['flag']
-    
-    bucket=file_datadict['bucket']
-    if flag=='new':
-        time_id=file_datadict['time']
-    else:
-        print(file_datadict['id'])
-        string_format=file_datadict['id']
-        string_format=str(string_format)
-        time_id=string_format.split('_')[0]
-    
-    s3_trigger_key='some_path/new_path/'+time_id+'/'
-        
+    my_parameters = json.loads(file_data)
     docker_obj=RunDocker()
-    run_docker_res=docker_obj.run_docker_image(time_id,s3_trigger_key,bucket,flag)
-    
-    
+    run_docker_res=docker_obj.run_docker_image(my_parameters)
     return run_docker_res
